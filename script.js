a3d1bf0a0995fb05b1df1ce18cadf872cd9ab442
yettsyknapp <yettsyjk@gmail.com>
2020-07-21 17:59:08 -0600

    working ES6 and JavaScript script

---
+++ script.js
@@ -0,0 +1,16 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+    clear(){
+        this.currentOperand ='';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete(){
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+}
